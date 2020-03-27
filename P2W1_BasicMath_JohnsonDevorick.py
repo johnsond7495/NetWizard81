0f458072eb7af4420bd8d42eac24be18a554005b
cti110 <62775888+johnsond7495@users.noreply.github.com>
2020-03-27 18:55:03 -0400

    Add files via upload
    Second Submission

---
+++ P2W1_BasicMath_JohnsonDevorick.py
@@ -0,0 +1,25 @@
+# This program demonstrates calculation of basic math
+# 27March2020
+# CTI-110 P2HW1 - Basic Math
+# Johnson Devorick
+
+# Enter the first number.
+first_number = 10
+
+# Enter the second number.
+second_number = 5
+
+# Calculate the total by adding sum of both numbers.
+FirstSum = first_number + second_number
+
+# Calculate by multiplying both numbers.
+SecondSum = first_number * second_number
+
+# Display the FirstSum.
+print ('Addition Total is', FirstSum)
+
+# Display the SecondSum.
+print ('Multiplication Total is', SecondSum)
+
+
+
