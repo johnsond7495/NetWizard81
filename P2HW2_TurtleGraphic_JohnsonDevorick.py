6c8f60d2069d9e17f83af3df9a01829270963b50
cti110 <62775888+johnsond7495@users.noreply.github.com>
2020-03-27 18:55:37 -0400

    Add files via upload
    Third Submission.

---
+++ P2HW2_TurtleGraphic_JohnsonDevorick.py
@@ -0,0 +1,25 @@
+#This program graph's creates a diamond patterned object
+#27 March 2020
+#CTI-110 P2HW2 - Turtle Graphic
+#Devorick Johnson
+
+import turtle
+>>>turtle.hideturtle()
+>>>turtle.fillcolor('black')
+>>>turtle.begin_fill()
+>>>turtle.left(180)
+>>>turtle.right(45)
+>>>turtle.foward(100)
+>>>turtle.right(90)
+>>>turtle.forward(100)
+>>>turtle.right(90)
+>>>turtle.forward(200)
+>>>turtle.left(90)
+>>>turtle.forward(100)
+>>>turtle.left(90)
+>>>turtle.forward(100)
+>>>turtle.left(90)
+>>>turtle.forward(200)
+>>>turtle.end_fill()
+
+        
