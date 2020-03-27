d1dc5105e6f4752c418823818c6ed7a0ce50e666
johnsond7495 <62775888+johnsond7495@users.noreply.github.com>
2020-03-27 18:45:47 -0400

    Add files via upload
    First Submission.

---
+++ P2T1_SalesPrediction_JohnsonDevorick.py
@@ -0,0 +1,13 @@
+# This python file calculates 23 percent of the total sales
+# 27March2020
+# CTI-110 P2T1 - Sales Prediction
+# Johnson Devorick
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales:'))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', profit)
