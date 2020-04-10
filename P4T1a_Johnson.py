3bf42c79cc4debef7fdeb072df9ab086098f5802
cti110 <62775888+johnsond7495@users.noreply.github.com>
2020-04-10 16:55:54 -0400

    Add files via upload
    9th Submission

---
+++ P4T1a_Johnson.py
@@ -0,0 +1,430 @@
+# This program creates 100 repeating squares
+# CTI-110 P4T1a - Shapes
+# 10 April 2020
+# Devorick Johnson
+import turtle
+
+# Draw the squares.
+for x in range(4):
+    turtle.left(90)
+    turtle.forward(5)
+
+for x in range(4):
+    turtle.left(90)
+    turtle.forward(10) #Each segment afterwards is increased by 5
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(15)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(20)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(25)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(30)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(35)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(40)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(45)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(50)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(55)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(60)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(65)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(70)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(75)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(80)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(85)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(90)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(95)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(100)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(105)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(110)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(115)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(120)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(125)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(130)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(135)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(140)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(145)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(150)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(155)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(160)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(165)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(170)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(175)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(180)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(185)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(190)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(195)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(200)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(205)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(210)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(215)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(220)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(225)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(230)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(235)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(240)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(245)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(250)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(255)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(260)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(265)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(270)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(275)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(280)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(285)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(290)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(295)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(300)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(305)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(310)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(315)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(320)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(325)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(330)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(335)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(340)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(345)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(350)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(355)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(360)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(365)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(370)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(375)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(380)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(385)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(390)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(395)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(400)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(405)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(410)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(415)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(420)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(425)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(430)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(435)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(440)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(445)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(450)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(455)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(460)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(465)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(470)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(475)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(480)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(485)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(490)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(495)
+
+for _ in range(4):
+    turtle.left(90)
+    turtle.forward(500)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
