2eb7acd646dfaafc7834d399113c4856606b0dc6
cti110 <62775888+johnsond7495@users.noreply.github.com>
2020-04-10 16:56:15 -0400

    Add files via upload
    10th Submission

---
+++ P4T1b_Johnson.py
@@ -0,0 +1,41 @@
+# This program create my initials with selected pensize and color
+# CTI-110 P4T1b - Initials
+# 10 April 2020
+# Devorick Johnson
+
+import turtle
+
+turtle.pencolor('blue')
+turtle.pensize(10)
+turtle.hideturtle()
+
+
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(20)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(20)
+turtle.left(45)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(128)
+
+turtle.showturtle()
+turtle.penup()
+turtle.goto(120, 127)
+turtle.pendown()
+
+turtle.left(90)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(123)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(20)
+
+
